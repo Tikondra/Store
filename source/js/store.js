92e6d8126ddceae28424dbfcf32e46f73c852294
Tikondra <tikondra47@gmail.com>
2020-03-15 16:20:17 +0400

    изменение оказа страниц, сброс счетчика, удаление из корзины

--- source/js/store.js
+++ source/js/store.js
@@ -2,12 +2,11 @@
 
 (function () {
   const available = [true, false];
-  const productCount = 15;
+  const productCount = 2;
   const debounceInterval = 500;
   const dataCount = 3000;
   let currentPage = 1;
   let arrayProducts = []; /** тестовый массив **/
-  let productList = document.querySelector('.products__list');
   let templateProduct = document.querySelector('#product')
       .content
       .querySelector('.products__item');
@@ -15,6 +14,7 @@
   let templateBasketItem = document.querySelector('#basket-item')
     .content
     .querySelector('.basket__item');
+  let productList = document.querySelector('.products__list');
   let basketList = document.querySelector('.basket__list');
   let lastPag = document.querySelector('.pagination__link--last');
   let pagPrev = document.querySelector('.pagination__btn--prev');
@@ -56,31 +56,59 @@
   }
   let lastPagValue = arrayProducts.length / productCount;
   lastPag.textContent = lastPagValue;
-  /**  удаление карточек товара **/
-  function delProductList() {
+  /**  скрытие карточек товара **/
+  function hideProductList() {
     let product = document.querySelectorAll('.products__item');
     product.forEach(function (item) {
-      item.remove();
+      item.classList.add('products__item--hide');
     })
   }
-  /** отрисовка карточек товара **/
-  function renderProductList(dataProduct, page) {
-    let fragment = document.createDocumentFragment();
+  /** показ карточек товара **/
+  function showProductList (page) {
+    let product = document.querySelectorAll('.products__item--hide');
     let rangeProduct = productCount * (page - 1);
 
     for (let i = rangeProduct; i < rangeProduct + productCount; i++) {
-      let product = templateProduct.cloneNode(true);
-      product.querySelector('img').src = dataProduct[i].image;
-      product.querySelector('img').alt = dataProduct[i].descr;
-      product.querySelector('.products__title').textContent = dataProduct[i].title;
-      product.querySelector('.products__price span').textContent = dataProduct[i].price;
-      product.querySelector('.products__text').textContent = dataProduct[i].descr;
-      product.setAttribute('data-id', dataProduct[i].id);
-      fragment.append(product);
+      product[i].classList.remove('products__item--hide');
     }
-    delProductList();
-    productList.append(fragment);
-    window.basket.makeEventAdd();
+  }
+  /** Добавление счетчика **/
+  function addCounter (item) {
+    let counter = makeCounter();
+
+    function onAddCounter (evt) {
+      if (evt.target.classList.contains('add__btn')) {
+        let count = counter();
+        window.basket.onAddBasket(evt, count);
+      }
+    }
+    item.addEventListener('click', onAddCounter);
+  }
+  /** отрисовка карточек товара **/
+  function renderProductList(dataProduct, page) {
+    let rangeProduct = productCount * (page - 1);
+    if (productList.querySelector('[data-num="' + (rangeProduct + 1) + '"]')) {
+      hideProductList();
+      showProductList(page);
+    } else {
+      let fragment = document.createDocumentFragment();
+
+      for (let i = rangeProduct; i < rangeProduct + productCount; i++) {
+        let product = templateProduct.cloneNode(true);
+        product.querySelector('img').src = dataProduct[i].image;
+        product.querySelector('img').alt = dataProduct[i].descr;
+        product.querySelector('.products__title').textContent = dataProduct[i].title;
+        product.querySelector('.products__price span').textContent = dataProduct[i].price;
+        product.querySelector('.products__text').textContent = dataProduct[i].descr;
+        product.setAttribute('data-id', dataProduct[i].id);
+        product.setAttribute('data-num', i + 1);
+        addCounter(product);
+
+        fragment.append(product);
+      }
+      hideProductList();
+      productList.append(fragment);
+    }
   }
   /**  пагинация **/
   (function pagination() {
@@ -153,9 +181,13 @@
     let basketEmpty = basket.querySelector('.basket__empty');
     let basketSum = basket.querySelector('.basket__sum b');
 
-    function getBasketSum (price) {
+    function getBasketSum (price, state, count) {
       let sumValue = Number(basketSum.textContent);
-      return sumValue + price;
+      if (state) {
+        return sumValue + price;
+      } else {
+        return sumValue - price * count;
+      }
     }
     /** добавление товара в корзину **/
     function addBasket (product, count) {
@@ -168,7 +200,7 @@
       basketItem.querySelector('img').src = productImg;
       basketItem.querySelector('.basket__title').textContent = productName;
       basketItem.querySelector('.basket__count').textContent = 'x' + count;
-
+      basketItem.addEventListener('click', onDelBasketItem);
       basketList.append(basketItem);
     }
     function updateBasketItem (itemId, itemCount) {
@@ -182,7 +214,7 @@
       let productId = productItem.dataset.id;
       let productPrice = Number(productItem.querySelector('.products__price span').textContent);
       let addMessage = productItem.querySelector('.add__message');
-      let addMessageCount = addMessage.querySelector('.add__message--count');
+      let addMessageCount = addMessage.querySelector('.add__message--count span');
 
       if (basketItems.length === 0) {
         addBasket(productItem, count);
@@ -197,32 +229,30 @@
         }
       }
 
-      basketSum.textContent = getBasketSum(productPrice);
-      addMessageCount.textContent = count + ' товаров';
+      basketSum.textContent = getBasketSum(productPrice, true);
+      addMessageCount.textContent = count;
       addMessage.classList.add('add__message--active');
       basketEmpty.classList.add('basket__empty--hide');
     }
     /** удаление позиции из корзины **/
-    function delBasketItem () {
+    function onDelBasketItem (evt) {
+      let basketItem = evt.currentTarget;
+      let delBtn = basketItem.querySelector('.basket__item-del');
+      let basketItemId = basketItem.dataset.id;
+      let item = productList.querySelector('[data-id="' + basketItemId + '"]');
+      let itemPrice = Number(item.querySelector('.products__price span').textContent);
+      let itemCount = Number(item.querySelector('.add__message--count span').textContent);
 
-    }
-    /** обработчик на добавление в корзину **/
-    function makeEventAdd () {
-      let cards = document.querySelectorAll('.products__item');
-
-      cards.forEach(function (btn) {
-        let counter = makeCounter();
-        btn.addEventListener('click', function (evt) {
-          if (evt.target.classList.contains('add__btn')) {
-            let count = counter();
-            onAddBasket(evt, count);
-          }
-        });
-      })
+      if (evt.target === delBtn) {
+        basketItem.remove();
+        basketSum.textContent = getBasketSum(itemPrice, false, itemCount);
+        item.querySelector('.add__message--count span').textContent = 0;
+        addCounter(item);
+      }
     }
 
     window.basket = {
-      makeEventAdd: makeEventAdd
+      onAddBasket: onAddBasket
     }
   })();
 
